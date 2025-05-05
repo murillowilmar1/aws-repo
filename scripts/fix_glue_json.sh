fb8347c0bf094bb5aadb3b340b248580799b7984
Wilmar Murillo Carmona <113046964+murillowilmar1@users.noreply.github.com>
2025-05-05 16:36:02 -0500

    Update fix_glue_json.sh

--- scripts/fix_glue_json.sh
+++ scripts/fix_glue_json.sh
@@ -2,22 +2,33 @@
 
 INPUT=$1
 OUTPUT=$2
-JOB_NAME=$(jq -r '.name' "$INPUT")
 
-jq --arg NAME "$JOB_NAME" '
-  del(.name) |
-  .Name = $NAME |
-  .ExecutionProperty.MaxConcurrentRuns = .executionProperty.maxConcurrentRuns |
-  .Command.Name = .command.name |
-  .Command.ScriptLocation = .command.scriptLocation |
-  .Command.PythonVersion = .command.pythonVersion |
-  .SourceControlDetails.Provider = .sourceControlDetails.provider |
-  .SourceControlDetails.Repository = .sourceControlDetails.repository |
-  .SourceControlDetails.Branch = .sourceControlDetails.branch |
-  .SourceControlDetails.Folder = .sourceControlDetails.folder |
-  del(
-    .executionProperty,
-    .command,
-    .sourceControlDetails
-  )
-' "$INPUT" > "$OUTPUT"
+jq '{
+  Name: .name,
+  JobMode: .jobMode,
+  Description: .description,
+  Role: .role,
+  ExecutionProperty: {
+    MaxConcurrentRuns: .executionProperty.maxConcurrentRuns
+  },
+  Command: {
+    Name: .command.name,
+    ScriptLocation: .command.scriptLocation,
+    PythonVersion: .command.pythonVersion
+  },
+  DefaultArguments: .defaultArguments,
+  MaxRetries: .maxRetries,
+  AllocatedCapacity: .allocatedCapacity,
+  Timeout: .timeout,
+  MaxCapacity: .maxCapacity,
+  GlueVersion: .glueVersion,
+  NumberOfWorkers: .numberOfWorkers,
+  WorkerType: .workerType,
+  ExecutionClass: .executionClass,
+  SourceControlDetails: {
+    Provider: .sourceControlDetails.provider,
+    Repository: .sourceControlDetails.repository,
+    Branch: .sourceControlDetails.branch,
+    Folder: .sourceControlDetails.folder
+  }
+}' "$INPUT" > "$OUTPUT"
